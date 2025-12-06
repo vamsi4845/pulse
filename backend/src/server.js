a49ac67718919e630c5c54ab16a067f57a4f37b6
vamsisai <vamsisai.karumuri@gmail.com>
2025-12-06 12:07:49 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,10 +18,7 @@ const io = initializeSocket(server);
 
 app.locals.io = io;
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
-}));
+app.use(cors());
 
 app.use(cookieParser());
 
